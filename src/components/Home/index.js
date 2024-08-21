cc1e87b88e664f9ed1b9786906ed34065894f1cc
Ryan Bridge <ryan@softwareforgood.com>
2024-08-21 10:59:24 -0400

    basic pot loaded

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,44 +1,24 @@
 /* eslint-disable react/no-unknown-property */
-/* eslint-disable no-sequences */
-/* eslint-disable react/jsx-closing-bracket-location */
-/* eslint-disable max-len */
-/* eslint-disable no-unused-vars */
-import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useFBX } from '@react-three/drei';
 
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef();
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false);
+function Scene() {
+  const fbx = useFBX('pot.fbx');
 
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => {
-    ref.current.rotation.x += delta;
-  });
-
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      ref={ref}
-      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'rebeccapurple'} />
-    </mesh>
-  );
+  return <primitive object={fbx} scale={0.005} />;
 }
 
 function Home() {
   return (
     <div className="wrapper">
       <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Box position={[0, 0, 0]} />
-        <OrbitControls />
+        <ambientLight intensity={0.9} />
+        <directionalLight color="white" position={[2, 3, 5]} />
+        <Suspense fallback={null}>
+          <Scene />
+          <OrbitControls />
+        </Suspense>
       </Canvas>
     </div>
   );
